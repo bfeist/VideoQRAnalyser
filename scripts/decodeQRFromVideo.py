b371a82da76b8b449b436315597cb59c74ee1fd2
Ben Feist <bf@benfeist.com>
2021-06-10 10:25:30 -0400

    Continued dev to handle found qrcode

--- scripts/decodeQRFromVideo.py
+++ scripts/decodeQRFromVideo.py
@@ -7,6 +7,10 @@ import dateutil.parser
 from multiprocessing import Process, Value, Array, Pool
 
 
+class BreakIt(Exception):
+    pass
+
+
 def f(thisFrame, currentFrame):
     if currentFrame % 100 == 0:
         print("Searching Frame# " + str(currentFrame), end="\r", flush=True)
@@ -21,38 +25,14 @@ def f(thisFrame, currentFrame):
         print("Data : ", barcode.data)
 
         if barcode.type == "QRCODE":
-            # parse encoded time into date
-            qrTimestamp = dateutil.parser.isoparse(barcode.data.decode("utf-8"))
-            print(
-                "Frame# "
-                + str(currentFrame)
-                + " QR Timestamp: "
-                + qrTimestamp.isoformat().replace("+00:00", "Z")
-                + "\n"
-            )
-
-            # get seconds value
-            # currSeconds = qrTimestamp.second
-
-            # if firstSeconds == 0:
-            #     firstSeconds = currSeconds
-
-            # # Detect if the QR time's second value has rolled over on this frame.
-            # # If so then this frame should be used to determine video start time using framerate math
-            # if currSeconds != firstSeconds:
-            #     secondsIntoVideo = currentFrame / fps
-            #     print("Time rollover detected " + str(secondsIntoVideo) + " seconds into the video")
-
-            #     # subtract seconds since beginning of video of current frame from the QR time to determine video start time
-            #     videoStartTime = qrTimestamp - datetime.timedelta(seconds=secondsIntoVideo)
-
-            #     print("Calculated video start time: " + str(videoStartTime.isoformat().replace("+00:00", "Z")))
+            return barcode.data
+    return 0
 
 
 if __name__ == "__main__":
     # test video with CODA Clocksync ISO time QR code in it.
     cam = cv2.VideoCapture("../vid/IMG_1722.MOV")
-    cam = cv2.VideoCapture("N:\\Projects\\NASA_CODA\\CODA_data\\RockYard\\2021-05-13-QuadView-EVA_20.26.55.MP4")
+    # cam = cv2.VideoCapture("N:\\Projects\\NASA_CODA\\CODA_data\\RockYard\\2021-05-13-QuadView-EVA_20.26.55.MP4")
 
     # get frames per second of video for use in start time calc
     fps = round(cam.get(cv2.CAP_PROP_FPS))
@@ -69,9 +49,35 @@ if __name__ == "__main__":
             # if frames remaining, continue reading frames
             if ret:
                 res = pool.apply_async(f, (frame, currentframe))  # runs in *only* one process
-                # print(res.get(timeout=1))
-                currentframe += 1
+                result = res.get(timeout=1)
+                if result != 0:
+                    # parse encoded time into date
+                    qrTimestamp = dateutil.parser.isoparse(result.decode("utf-8"))
+                    print(
+                        "Frame# "
+                        + str(currentframe)
+                        + " QR Timestamp: "
+                        + qrTimestamp.isoformat().replace("+00:00", "Z")
+                        + "\n"
+                    )
+                    # get seconds value
+                    currSeconds = qrTimestamp.second
+                    if firstSeconds == 0:
+                        firstSeconds = currSeconds
 
+                    # Detect if the QR time's second value has rolled over on this frame.
+                    # If so then this frame should be used to determine video start time using framerate math
+                    if currSeconds != firstSeconds:
+                        secondsIntoVideo = currentframe / fps
+                        print("Time rollover detected " + str(secondsIntoVideo) + " seconds into the video")
+
+                        # subtract seconds since beginning of video of current frame from the QR time to determine video start time
+                        videoStartTime = qrTimestamp - datetime.timedelta(seconds=secondsIntoVideo)
+
+                        print("Calculated video start time: " + str(videoStartTime.isoformat().replace("+00:00", "Z")))
+                    raise BreakIt
+
+                currentframe += 1
             else:
                 break
 
