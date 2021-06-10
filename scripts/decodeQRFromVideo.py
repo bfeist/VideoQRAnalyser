cd08c95d5a5746ee8504ea3614ab32244c2ad233
Ben Feist <bf@benfeist.com>
2021-06-10 16:03:09 -0400

    WIP

--- scripts/decodeQRFromVideo.py
+++ scripts/decodeQRFromVideo.py
@@ -38,37 +38,21 @@ def lookForQRcodes(thisFrame, currentFrameNumber):
 
 
 def QRcodeWorkerCallback(returnStatus: ReturnStatus):
-    global startTimeFound
-    global firstSeconds
-    if returnStatus.qrFound != False and startTimeFound == False:
+    global firstQRFoundFrameNumber
+    if returnStatus.qrFound != False and firstQRFoundFrameNumber == 0:
         # parse encoded time into date
         qrTimestamp = dateutil.parser.isoparse(returnStatus.qrData.decode("utf-8"))
         print(
             "Frame# "
             + str(returnStatus.frameNumber)
-            + " QR Timestamp: "
+            + " Pool worker found QR Timestamp: "
             + qrTimestamp.isoformat().replace("+00:00", "Z")
         )
-        # get seconds value
-        currSeconds = qrTimestamp.second
-        if firstSeconds == 0:
-            firstSeconds = currSeconds
-
-        # Detect if the QR time's second value has rolled over on this frame.
-        # If so then this frame should be used to determine video start time using framerate math
-        if currSeconds != firstSeconds:
-            secondsIntoVideo = currentFrame / fps
-            print("Time rollover detected " + str(secondsIntoVideo) + " seconds into the video")
-
-            # subtract seconds since beginning of video of current frame from the QR time to determine video start time
-            videoStartTime = qrTimestamp - datetime.timedelta(seconds=secondsIntoVideo)
-
-            print("Calculated video start time: " + str(videoStartTime.isoformat().replace("+00:00", "Z")))
-            startTimeFound = True
+        firstQRFoundFrameNumber = returnStatus.frameNumber
 
 
 if __name__ == "__main__":
-    startTimeFound = False
+    firstQRFoundFrameNumber = 0
     currentFrame = 0
     firstSeconds = 0
 
@@ -81,24 +65,74 @@ if __name__ == "__main__":
 
     with Pool(processes=8) as pool:
         # Loop through all of the frames in the video
+        while True:
+            # reading from frame
+            ret, frame = cam.read()
 
-        try:
-            while True:
-                # reading from frame
-                ret, frame = cam.read()
+            # if frames remaining, continue reading frames
+            if ret:
+                # send frame to pool worker process
+                res = pool.apply_async(lookForQRcodes, (frame, currentFrame), callback=QRcodeWorkerCallback)
+                currentFrame += 1
+            else:
+                break
+            if firstQRFoundFrameNumber != 0:
+                break
 
-                # if frames remaining, continue reading frames
-                if ret:
-                    # send frame to pool worker process
-                    res = pool.apply_async(lookForQRcodes, (frame, currentFrame), callback=QRcodeWorkerCallback)
-                    currentFrame += 1
-                else:
-                    break
-                if startTimeFound == True:
-                    raise BreakIt
-        except BreakIt:
-            print(res.get(timeout=1))
-            print("Search terminated")
-            pass
+    # Now we have the frame number the first QR code image was found using the parallel processing method above
+    # Now use single-thread method to determine precise video start time
+
+    # start reading at frame number where the first QR code was found
+    currentFrame = firstQRFoundFrameNumber
+
+    cam.set(cv2.CAP_PROP_POS_FRAMES, currentFrame)
+    try:
+        while True:
+            # reading from frame
+            ret, frame = cam.read()
+
+            # if frames remaining, continue reading frames
+            if ret:
+                if currentFrame % 10 == 0:
+                    print("Single thread - searching Frame# " + str(currentFrame), end="\r", flush=True)
+                # find the barcodes in the frame and decode each of the barcodes
+                decodedBarcodes = pyzbar.decode(frame, symbols=[ZBarSymbol.QRCODE])
+
+                # loop over the detected barcodes in the frame (there should only be one)
+                for barcode in decodedBarcodes:
+                    # If QR found, then return its data
+                    if barcode.type == "QRCODE":
+                        qrTimestamp = dateutil.parser.isoparse(barcode.decode("utf-8"))
+                        print(
+                            "Frame# "
+                            + str(currentFrame)
+                            + " QR Timestamp: "
+                            + qrTimestamp.isoformat().replace("+00:00", "Z")
+                        )
+                        # get seconds value
+                        currSeconds = qrTimestamp.second
+                        if firstSeconds == 0:
+                            firstSeconds = currSeconds
+
+                        # Detect if the QR time's second value has rolled over on this frame.
+                        # If so then this frame should be used to determine video start time using framerate math
+                        if currSeconds != firstSeconds:
+                            secondsIntoVideo = currentFrame / fps
+                            print("Time rollover detected " + str(secondsIntoVideo) + " seconds into the video")
+
+                            # subtract seconds since beginning of video of current frame from the QR time to determine video start time
+                            videoStartTime = qrTimestamp - datetime.timedelta(seconds=secondsIntoVideo)
+
+                            print(
+                                "Calculated video start time: " + str(videoStartTime.isoformat().replace("+00:00", "Z"))
+                            )
+                            raise BreakIt
+
+                currentFrame += 1
+    except BreakIt:
+        print(res.get(timeout=1))
+        print("Search terminated")
+        pass
+
     # Release all space once done
     cam.release()
